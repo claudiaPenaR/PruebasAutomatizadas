ec5e7b1f1d4a3de41fc2aa320dde5f6b3fa97311
claudiaPenaR <113155386+claudiaPenaR@users.noreply.github.com>
2024-11-24 02:19:45 -0300

    Update LoginSteps.java

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -2,9 +2,9 @@ package stepDefinitions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -18,39 +18,31 @@ public class LoginSteps {
 
     @Given("el usuario se encuentra en la página de login")
     public void el_usuario_se_encuentra_en_la_página_de_login() {
-        // Configuración de la ruta del ChromeDriver
-    	 // Configuración de GeckoDriver
-        System.setProperty("webdriver.gecko.driver", "C:/Users/HP/Drivers/geckodriver.exe"); // Cambia la ruta según corresponda
-
-        // Opciones para Firefox (opcional)
-        FirefoxOptions options = new FirefoxOptions();
-        driver = new FirefoxDriver(options); // Inicializar Firefox con las opciones
-
-        // Abrir la URL de la página de login
-        driver.get("https://the-internet.herokuapp.com/login"); // Cambia esta URL por la de tu aplicación de pruebas
+        System.setProperty("webdriver.gecko.driver", "C:/Drivers/geckodriver.exe"); // Ajusta la ruta
+        driver = new FirefoxDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
     }
 
     @When("ingresa el nombre de usuario y la contraseña válidos")
     public void ingresa_el_nombre_de_usuario_y_la_contraseña_válidos() {
-        // Datos simulados (puedes sustituirlos por datos de una base de datos si es necesario)
-        String username = "usuarioValido";
-        String password = "contraseñaValida";
+        String username = "tomsmith";
+        String password = "SuperSecretPassword!";
 
-        // Interactuar con los elementos de la página
         driver.findElement(By.id("username")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("submit")).click(); // Botón para enviar el formulario
+
+        // Esperar hasta que el botón de login sea clickeable
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("login-button")));
+        
+        // Hacer clic en el botón de login
+        driver.findElement(By.id("login-button")).click();
     }
 
     @Then("debe ser redirigido a la página principal")
     public void debe_ser_redirigido_a_la_página_principal() {
-        // URL esperada después del login exitoso
-        String expectedUrl = "https://the-internet.herokuapp.com/secure"; // Cambia esta URL según tu caso
-
-        // Verificar que la URL actual sea la esperada
+        String expectedUrl = "https://the-internet.herokuapp.com/secure";
         assertEquals(expectedUrl, driver.getCurrentUrl());
-
-        // Cerrar el navegador al finalizar
         driver.quit();
     }
 }
